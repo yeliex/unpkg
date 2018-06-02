8d3378a536ad312d4398feae89601f0c1d595e7f
Michael Jackson <mj@mjackson.me>
2018-06-02 06:35:40 -0700

    Remove throng

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 const path = require("path");
-const throng = require("throng");
 
-const createServer = require("./server/createServer");
-const createDevServer = require("./server/createDevServer");
 const config = require("./server/config");
 
-function startServer(id) {
-  const server =
-    process.env.NODE_ENV === "production"
-      ? createServer(
-          path.resolve(__dirname, "public"),
-          path.resolve(__dirname, "server/stats.json")
-        )
-      : createDevServer(
-          path.resolve(__dirname, "public"),
-          require("./webpack.config"),
-          config.origin
-        );
+let server;
 
-  server.listen(config.port, () => {
-    console.log(
-      "Server #%s listening on port %s, Ctrl+C to stop",
-      id,
-      config.port
-    );
-  });
+if (process.env.NODE_ENV === "production") {
+  const createServer = require("./server/createServer");
+
+  server = createServer(
+    path.resolve(__dirname, "public"),
+    path.resolve(__dirname, "server/stats.json")
+  );
+} else {
+  const createServer = require("./server/createDevServer");
+
+  server = createDevServer(
+    path.resolve(__dirname, "public"),
+    require("./webpack.config"),
+    config.origin
+  );
 }
 
-throng({
-  workers: process.env.WEB_CONCURRENCY || 1,
-  lifetime: Infinity,
-  grace: 25000,
-  start: startServer
+server.listen(config.port, () => {
+  console.log("Server listening on port %s, Ctrl+C to stop", config.port);
 });
